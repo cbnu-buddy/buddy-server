f19370c0163911f9c36fc0c7846fa22465387c8d
jenny <dpwb12@naver.com>
2024-06-06 22:03:35 +0900

    [fix]: 나의 파티 목록 조회 기능 수정

--- src/main/java/com/example/dto/response/MyPartyInfoResponse.java
+++ src/main/java/com/example/dto/response/MyPartyInfoResponse.java
@@ -4,8 +4,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
-
 @Getter
 @Setter
 @Builder
@@ -36,5 +34,6 @@ public class MyPartyInfoResponse {
         private String startDate;
         private int durationMonth;
         private String endDate;
+        private boolean progressStatus;
     }
 }
