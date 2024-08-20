be53b23d2ead5aded20eaf192b3069856d544202
ttrlf <qmdlxlr@gmail.com>
2024-08-20 10:27:23 +0900

    [fix] 파티 매칭 완료시 이메일 전송 서비스 로직 수정 및 기능 개선

--- src/main/java/com/example/service/email/EmailService.java
+++ src/main/java/com/example/service/email/EmailService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 
@@ -16,6 +17,8 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+
+    @Async
     public void sendEmail(String to, String subject, String htmlContent) {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         try {
